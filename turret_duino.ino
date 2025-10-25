cc81a1003fb2509ceff6af786a41fecbf6ae2e13
Nils Collins <nilscollins@Nilss-MacBook-Pro.local>
2025-10-25 13:25:42 +0200

    smoothing

--- turret_duino.ino
+++ turret_duino.ino
@@ -21,11 +21,13 @@ void setup() {
   Serial.begin(9600);
   servoX.attach(servoXPin);
   servoY.attach(servoYPin);
-  
+
   // Center servos
   servoX.write(90);
   servoY.write(90);
   delay(1000);
+  
+  Serial.println("Arduino Ready - Servos Centered");
 }
 
 void loop() {
@@ -33,44 +35,81 @@ void loop() {
     String data = Serial.readStringUntil('\n');
     data.trim();
     
+    Serial.print("Received: ");
+    Serial.println(data);
+
     int commaIndex = data.indexOf(',');
     if (commaIndex > 0) {
       String xStr = data.substring(0, commaIndex);
       String yStr = data.substring(commaIndex + 1);
-      
+
       float commandX = xStr.toFloat();
       float commandY = yStr.toFloat();
       
+      Serial.print("Commands - X: ");
+      Serial.print(commandX);
+      Serial.print(" Y: ");
+      Serial.println(commandY);
+
       // Update target positions
       targetX += commandX;
       targetY += commandY;
-      
+
       // Constrain targets
       targetX = constrain(targetX, 0, 180);
       targetY = constrain(targetY, 0, 180);
+      
+      Serial.print("New Targets - X: ");
+      Serial.print(targetX);
+      Serial.print(" Y: ");
+      Serial.println(targetY);
     }
   }
-  
+
   // Smooth movement towards target
   float deltaX = targetX - posX;
   float deltaY = targetY - posY;
   
+  Serial.print("Deltas - X: ");
+  Serial.print(deltaX);
+  Serial.print(" Y: ");
+  Serial.println(deltaY);
+
   // Apply deadzone to reduce micro-movements
   if (abs(deltaX) > deadzone) {
     posX += deltaX * smoothing;
-  }
-  if (abs(deltaY) > deadzone) {
-    posY += deltaY * smoothing;
+    Serial.print("Moving X from ");
+    Serial.print(posX - deltaX * smoothing);
+    Serial.print(" to ");
+    Serial.println(posX);
+  } else {
+    Serial.println("X in deadzone");
   }
   
+  if (abs(deltaY) > deadzone) {
+    posY += deltaY * smoothing;
+    Serial.print("Moving Y from ");
+    Serial.print(posY - deltaY * smoothing);
+    Serial.print(" to ");
+    Serial.println(posY);
+  } else {
+    Serial.println("Y in deadzone");
+  }
+
   // Constrain current positions
   posX = constrain(posX, 0, 180);
   posY = constrain(posY, 0, 180);
-  
+
   // Write to servos
   servoX.write((int)posX);
   servoY.write((int)posY);
   
+  Serial.print("Final Positions - X: ");
+  Serial.print((int)posX);
+  Serial.print(" Y: ");
+  Serial.println((int)posY);
+  Serial.println("---");
+
   // Small delay for smooth operation
   delay(20);  // 50Hz update rate
 }
